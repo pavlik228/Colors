85a247e1b5a05bb7ddc827acef23026ddd504fa0
Pavel Aushev <ausevpavel@gmail.com>
2019-11-27 19:37:41 +0300

    Initial Commit

---
+++ Colors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by Pavel Aushev on 27.11.2019.
+//  Copyright © 2019 Павел Аушев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
